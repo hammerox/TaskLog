661f7a4e98614aee91d1c83930c6c4fa53d9dc27
mcustodio <mauricioramalho2@gmail.com>
2018-04-16 03:53:48 -0300

    FolderNameDialog: Added default text.

--- app/src/main/java/com/mcustodio/tasklog/view/folders/FolderNameDialog.kt
+++ app/src/main/java/com/mcustodio/tasklog/view/folders/FolderNameDialog.kt
@@ -3,8 +3,9 @@ package com.mcustodio.tasklog.view.folders
 import android.app.Activity
 import com.afollestad.materialdialogs.MaterialDialog
 import com.mcustodio.tasklog.R
-import com.mcustodio.tasklog.model.folder.Folder
+import com.mcustodio.tasklog.utils.toString
 import kotlinx.android.synthetic.main.dialog_folders.view.*
+import java.util.*
 
 class FolderNameDialog(activity: Activity, onSelect: ((String) -> Unit)) {
 
@@ -21,6 +22,20 @@ class FolderNameDialog(activity: Activity, onSelect: ((String) -> Unit)) {
 
 
     fun show() {
+        val now = Calendar.getInstance().time
+        dialog.customView?.edit_folderdesc_name?.setText(now.toString("dd/MM - '$weekName'"))
         dialog.show()
+    }
+
+
+    private val weekName = when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+        0 -> "Sab"
+        1 -> "Dom"
+        2 -> "Seg"
+        3 -> "Ter"
+        4 -> "Qua"
+        5 -> "Qui"
+        6 -> "Sex"
+        else -> "???"
     }
 }
