89cdda11853caa3d21a08d6656fabf6d111fa520
mcustodio <mauricioramalho2@gmail.com>
2018-08-17 00:06:20 -0300

    FoldersActivity: Now is possible to export database to CSV.

--- app/src/main/java/com/mcustodio/tasklog/view/App.kt
+++ app/src/main/java/com/mcustodio/tasklog/view/App.kt
@@ -1,13 +1,14 @@
 package com.mcustodio.tasklog.view
 
 import android.app.Application
+import android.support.multidex.MultiDexApplication
 import com.facebook.stetho.Stetho
 import com.mcustodio.tasklog.model.AppDatabase
 
 /**
  * Created by logonrm on 17/02/2018.
  */
-class App : Application() {
+class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/mcustodio/tasklog/view/folders/FoldersActivity.kt
+++ app/src/main/java/com/mcustodio/tasklog/view/folders/FoldersActivity.kt
@@ -1,16 +1,27 @@
 package com.mcustodio.tasklog.view.folders
 
+import android.Manifest
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import com.afollestad.materialdialogs.MaterialDialog
 import com.mcustodio.tasklog.R
 import com.mcustodio.tasklog.model.folder.Folder
 import com.mcustodio.tasklog.utils.Preferences
 import com.mcustodio.tasklog.view.main.MainActivity
 import kotlinx.android.synthetic.main.activity_folders.*
+import android.widget.Toast
+import java.lang.Exception
+import android.os.Build
+import com.mcustodio.tasklog.model.AppDatabase
+import com.opencsv.CSVWriter
+import java.io.*
+
 
 class FoldersActivity : AppCompatActivity() {
 
@@ -24,6 +35,7 @@ class FoldersActivity : AppCompatActivity() {
         setContentView(R.layout.activity_folders)
         setView()
         observeFolders()
+        requestWritePermission()
     }
 
 
@@ -33,12 +45,34 @@ class FoldersActivity : AppCompatActivity() {
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_folder, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuitem_folder_export -> export()
+        }
+        return true
+    }
+
+
     private fun setView() {
         recycler_folders.layoutManager = LinearLayoutManager(this)
         recycler_folders.adapter = adapter
         setAdapterClickListeners()
         fab_folders_add.setOnClickListener {
             FolderNameDialog(this, onNameSet).show()
+        }
+    }
+
+
+    private fun requestWritePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            requestPermissions(permissions, 0)
         }
     }
 
@@ -81,5 +115,42 @@ class FoldersActivity : AppCompatActivity() {
 
     private val onNameSet : (String) -> Unit = { name ->
         viewModel.createFolder(name)
+    }
+
+
+    // todo - Refatorar código para Repository
+    // todo - Enviar para email
+    private fun export() {
+
+        val exportDir = File(Environment.getExternalStorageDirectory(), "")
+        if (!exportDir.exists()) exportDir.mkdirs()
+
+        val file = File(exportDir, "csvname.csv")
+        try {
+            file.createNewFile()
+            val csvWrite = CSVWriter(FileWriter(file))
+            val db = AppDatabase.getFrom(this).openHelper.readableDatabase
+            val cursor = db.query("SELECT * FROM Task",null)
+            csvWrite.writeNext(cursor.columnNames)
+            while(cursor.moveToNext())
+            {
+                //Which column you want to exprort
+                val list = arrayListOf<String>()
+                for (i in 0..(cursor.columnCount - 1)) {
+                    list.add(cursor.getString(i))
+                }
+                csvWrite.writeNext(list.toTypedArray())
+            }
+            csvWrite.flush()
+            csvWrite.close()
+            cursor.close()
+
+            Toast.makeText(this, "SUCESSO", Toast.LENGTH_LONG).show()
+        }
+        catch(sqlEx: Exception)
+        {
+            requestWritePermission()
+            Toast.makeText(this, sqlEx.message, Toast.LENGTH_LONG).show()
+        }
     }
 }
