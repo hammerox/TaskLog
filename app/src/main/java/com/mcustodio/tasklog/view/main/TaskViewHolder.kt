689c2912438e823ad661a27c0048d2cb73006f98
mcustodio <mauricioramalho2@gmail.com>
2018-08-26 17:42:36 -0300

    TaskActivity: Added ellipsis animation when task is running. gradle: Added Lottie.

--- app/src/main/java/com/mcustodio/tasklog/view/main/TaskViewHolder.kt
+++ app/src/main/java/com/mcustodio/tasklog/view/main/TaskViewHolder.kt
@@ -19,6 +19,7 @@ class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val date = view.text_counteritem_date
     private val description = view.text_counteritem_description
     private val diff = view.text_counteritem_diff
+    private val ellipsisAnim = view.anim_counteritem_ellipsis
 
     private val defaultColor = ContextCompat.getColor(view.context, android.R.color.primary_text_dark)
     private val darkColor = ContextCompat.getColor(view.context, R.color.darkBackground)
@@ -26,6 +27,7 @@ class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     fun setView(task: Task, nextTask: Task?) {
         val cardColor = if (task.isActive()) defaultColor else darkColor
+        val showAnimation = nextTask == null && task.isActive()
         card.setCardBackgroundColor(cardColor)
 
         date.text = task.startDate?.toString("HH'h'mm") ?: ""
@@ -33,6 +35,9 @@ class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         description.text = task.description
 
         diff.text = timeDifference(task, nextTask)
+        diff.switchVisibility(!showAnimation)
+
+        ellipsisAnim.switchVisibility(showAnimation)
     }
 
 
